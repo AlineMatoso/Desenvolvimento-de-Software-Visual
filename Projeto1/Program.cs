acf2d0fba83d5f6c42bd86eb388e2ce7e8de4f96
ALINE MATOSO DE LIMA <aline.lima03@cs.up.edu.br>
2025-08-06 21:32:58 -0300

    segundo commit , exercicios feitos ate o 9

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,25 +1,88 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+namespace Projeto1{
+     public class Program {
+    static void Main() {
+        
+        
+        /*Peça para o usuário digitar sua idade, altura, e nome. Depois, exiba essas informações no console formatadas de forma amigável.
+        
+        Console.WriteLine("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite sua altura: ");
+        double altura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Nome : " + nome + " Idade:  " + idade + " Altura: "+ altura); */
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        /*Crie duas variáveis inteiras chamadas a e b, com os valores 10 e 20. Some esses dois valores e exiba o resultado. Depois, crie duas variáveis do tipo double e faça o mesmo.
+        int a = 10;
+        int b = 20;
+        int soma  = a + b;
+        Console.WriteLine(soma);
 
-var app = builder.Build();
+        double c = 13.4;
+        double d = 10.6;
+        double soma2 = c + d;
+        Console.WriteLine(soma2);
+        */
+        /*Declare as seguintes variáveis:
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        nome (string)
+        idade (int)
+        altura (double)
+        gostaDeProgramar (bool)
+        Depois, exiba no console uma frase como: "Olá, meu nome é João, tenho 30 anos, 1.80m de altura e é verdade que gosto de programar: True"
+
+        Exiba a altura com 2 casas decimais.
+
+        string nome;
+        int idade;
+        double altura;
+        bool gostaDeProgramar = true;
+
+        Console.WriteLine("Digite seu nome: ");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade: ");
+        idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite sua altura: ");
+        altura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Gosta de programar? ");
+        Console.WriteLine("0 - NÃO");
+        Console.WriteLine("1 - SIM");
+        int resposta = int.Parse(Console.ReadLine());
+        while (resposta >= 0 || resposta <= 1){
+        if(resposta == 0){
+            gostaDeProgramar = false;
+        } else if(resposta == 1){
+            gostaDeProgramar = true;
+        } 
+        } 
+ 
+        Console.WriteLine("Nome: " + nome + " Idade: " + idade + " Altura: " + altura + " Gosta de programar? " + gostaDeProgramar);
+    }
+    } */
+
+
+ /* Solicite a nota de um aluno (de 0 a 10). Se a nota for maior ou igual a 6, exiba "Aprovado", caso contrário, exiba "Reprovado" 
+
+    Console.WriteLine("Digite sua nota: ");
+    double nota = double.Parse(Console.ReadLine());
+    if(nota<=5){
+        Console.WriteLine(" Aluno Reprovado. ");
+    } else if(nota>=6){
+        Console.WriteLine(" Aluno Aprovado. ");
+    }
+*/
+
+    /*Um sistema de entrada de festa deve permitir apenas pessoas com idade igual ou maior que 18 e com convite. Solicite a idade e
+     se a pessoa tem convite (true ou false). Exiba se a pessoa pode entrar ou não. */
+   
+   
+   
+    }
+} 
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
